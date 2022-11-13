621a89288f7063f5f3c061c2ea975c0ee1fe440a
TGOLD <thomaswgold90@gmail.com>
2022-11-13 12:37:04 -0700

    about minor fix

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,8 +4,8 @@ import React from "react";
 
 export default function About() {
   return (
-    <section className='text-white px-10 xl:text-xl lg:px-6 md:px-4 sm:px-0 content grid grid-cols-2 gap-10 xl:grid-cols-1 lg:grid-cols-1 md:grid-cols-1 sm:grid-cols-1'>
-      <div className='.about-big col-span-2 xl:col-span-1 lg:col-span-1 md:col-span-1 sm:col-span-1 text-6xl p-10 text-center'>
+    <section className='text-white px-10 xl:text-xl lg:px-6 md:px-4 sm:px-0 content grid grid-cols-2 gap-10 xl:grid-cols-2 lg:grid-cols-1 md:grid-cols-1 sm:grid-cols-1'>
+      <div className='.about-big col-span-2 xl:col-span-2 lg:col-span-1 md:col-span-1 sm:col-span-1 text-6xl p-10 text-center'>
         Hello. My name is Tom{" "}
         <strong className='font-extrabold gold-glow'>Gold</strong>
       </div>
